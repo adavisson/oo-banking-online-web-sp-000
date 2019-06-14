62704615dc7bffff98c1dd166d27e423811d4dc1
Andrew Davisson <akdavisson4@gmail.com>
2019-06-14 14:44:14 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,4 +16,12 @@ class BankAccount
     "Your balance is $#{self.balance}."
   end
 
+  def valid?
+    if self.status = "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
 end
